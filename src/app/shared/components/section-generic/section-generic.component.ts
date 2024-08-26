9c2751441dd80f45c1cb1e21fb104757712d0671
Isabel <isabel.montes2607@gmail.com>
2024-08-26 11:51:57 -0600

    STFireHub Full images api/kubernetessERR

--- src/app/shared/components/section-generic/section-generic.component.ts
+++ src/app/shared/components/section-generic/section-generic.component.ts
@@ -14,14 +14,14 @@ export class SectionGenericComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit(): void {
-    // Puedes dejar esta consola para depuración, pero no es necesario hacer nada más aquí
+   
     console.log('ngOnInit - DataMovies:', this.dataMovies);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.dataMovies && this.dataMovies) {
       console.log('ngOnChanges - DataMovies:', this.dataMovies);
-      // Si necesitas realizar alguna acción específica cuando cambian los datos, hazlo aquí
+     
     }
   }
 }
